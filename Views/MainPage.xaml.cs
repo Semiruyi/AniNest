27096a96b10baa4566a0eadb5d8d9f82ec4055fb
wyy <w17395375229@gmail.com>
2026-04-28 11:36:02 +0800

    拖拽蓝色指示线增加淡入淡出效果
    Signed-off-by: wyy <w17395375229@gmail.com>

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -387,8 +387,7 @@ public partial class MainPage : System.Windows.Controls.UserControl
     private void HideInsertionIndicator()
     {
         if (_insertionAdorner == null) return;
-        var layer = AdornerLayer.GetAdornerLayer(FolderList);
-        layer?.Remove(_insertionAdorner);
+        _insertionAdorner.Hide();
         _insertionAdorner = null;
     }
 
@@ -443,6 +442,16 @@ public class InsertionAdorner : Adorner
     private bool _visible;
     private System.Windows.Rect _rect;
 
+    private static readonly DependencyProperty AnimOpacityProperty =
+        DependencyProperty.Register("AnimOpacity", typeof(double), typeof(InsertionAdorner),
+            new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+    private double AnimOpacity
+    {
+        get => (double)GetValue(AnimOpacityProperty);
+        set => SetValue(AnimOpacityProperty, value);
+    }
+
     private static readonly SolidColorBrush InsertionBrush = new(System.Windows.Media.Color.FromRgb(0x4A, 0x9E, 0xFF));
 
     public InsertionAdorner(UIElement adornedElement) : base(adornedElement)
@@ -453,13 +462,42 @@ public class InsertionAdorner : Adorner
     public void ShowAt(double x, double y, double height)
     {
         _rect = new System.Windows.Rect(x, y, 4, height);
-        _visible = true;
+
+        if (!_visible)
+        {
+            _visible = true;
+            var anim = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(700))
+            {
+                EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+            };
+            BeginAnimation(AnimOpacityProperty, anim);
+        }
+
         InvalidateVisual();
+    }
+
+    public void Hide()
+    {
+        if (!_visible) return;
+        _visible = false;
+
+        var anim = new DoubleAnimation(AnimOpacity, 0, TimeSpan.FromMilliseconds(700))
+        {
+            EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+        };
+        anim.Completed += (_, _) =>
+        {
+            var layer = AdornerLayer.GetAdornerLayer(AdornedElement);
+            layer?.Remove(this);
+        };
+        BeginAnimation(AnimOpacityProperty, anim);
     }
 
     protected override void OnRender(DrawingContext drawingContext)
     {
-        if (!_visible) return;
+        if (!_visible && AnimOpacity <= 0) return;
+        drawingContext.PushOpacity(AnimOpacity);
         drawingContext.DrawRoundedRectangle(InsertionBrush, null, _rect, 2, 2);
+        drawingContext.Pop();
     }
 }
