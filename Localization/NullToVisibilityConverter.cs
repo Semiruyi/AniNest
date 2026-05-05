724df5bb0705c410abe9ca93df8e6d149e42337d
wyy <w17395375229@gmail.com>
2026-05-05 17:30:46 +0800

    卡片元素精简

---
+++ Localization/NullToVisibilityConverter.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LocalPlayer.Localization;
+
+public sealed class NullToVisibilityConverter : IValueConverter
+{
+    public Visibility NullValue { get; set; } = Visibility.Collapsed;
+    public Visibility NotNullValue { get; set; } = Visibility.Visible;
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value == null ? NullValue : NotNullValue;
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
