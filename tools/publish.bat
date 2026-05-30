54c62495036ad8d9d53c8f10b5c8e23e09d15f63
wyy <w17395375229@gmail.com>
2026-05-30 17:39:44 +0800

    clear obsoleted file

--- tools/publish.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-chcp 65001 >nul
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "[Console]::OutputEncoding = [System.Text.Encoding]::UTF8; & '%~dp0publish.ps1' -Runtime win-x64 -Configuration Release"
-
-endlocal
